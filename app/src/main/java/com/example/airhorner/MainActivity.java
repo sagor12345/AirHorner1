ae196e7f787508b72f0f35ecb6755fc56fa28ff8
MD. SHAMIM HOSSAIN <mdshamimh529@gmail.com>
2019-11-17 11:22:08 +0600

    add commet

---
+++ app/src/main/java/com/example/airhorner/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.airhorner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    MediaPlayer player;
+    AnimationDrawable animationDrawable;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        super.onCreate( savedInstanceState );
+
+        setContentView( R.layout.activity_main );
+
+        button = findViewById( R.id.btn );
+
+        ImageView img = findViewById(R.id.image);
+        img.setBackgroundResource(R.drawable.anim);
+        animationDrawable = (AnimationDrawable) img.getBackground();
+
+        final int[] sound= {R.raw.horn,R.raw.sound1,R.raw.sound2};
+
+        button.setOnTouchListener( new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+
+                if (event.getAction()== MotionEvent.ACTION_DOWN) {
+                    animationDrawable.start();
+
+                    Random random = new Random();
+                    int low=0;
+                    int high=3;
+                    int rnd = random.nextInt(high-low)+low;
+
+                    player= MediaPlayer.create( MainActivity.this,sound[rnd] );
+                    player.start();
+
+                    v.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                    v.invalidate();
+                }
+                else if(event.getAction()==MotionEvent.ACTION_UP){
+                    animationDrawable.stop();
+                    animationDrawable.selectDrawable(0);
+                    player.stop();
+                    player.release();
+
+                    v.getBackground().clearColorFilter();
+                    v.invalidate();
+                }
+                return true;
+
+            }
+
+        } );
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.about_us:
+             //   addSomething();
+                Toast.makeText(MainActivity.this,"About Us",Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.action_settings:
+              //  startSettings();
+                Toast.makeText(MainActivity.this,"About Us",Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.share:
+                //  startshare();
+                //Toast.makeText(MainActivity.this,"About Us",Toast.LENGTH_LONG).show();
+                Intent shareIntent =   new Intent(android.content.Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT,"Air Horner Apps");
+                startActivity(Intent.createChooser(shareIntent, "Share via"));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
+// Complte
---
+++ app/src/main/java/com/example/airhorner/Splash.java
@@ -0,0 +1,58 @@
+package com.example.airhorner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class Splash extends AppCompatActivity {
+    ProgressBar progressBar;
+    int progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        progressBar= findViewById(R.id.pBar);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                splash();
+                GoHome();
+            }
+        });
+        thread.start();
+
+    }
+
+    public  void splash(){
+        for (progress =20;progress<=100;progress=progress+20){
+            try {
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    public  void  GoHome(){
+        Intent i = new Intent(Splash.this,MainActivity.class);
+        startActivity(i);
+        finish();
+
+    }
+}
